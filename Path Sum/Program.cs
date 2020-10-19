c377ef47544a855009d0da70b4d83f139d9664f8
Jaydeep Shil <ED8A@eurofins.com>
2020-10-19 21:29:44 +0530

    Add project files.

---
+++ Path Sum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Path_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class TreeNode
+        {
+            public int val { get; set; }
+            public TreeNode left { get; set; }
+            public TreeNode right { get; set; }
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        // https://leetcode.com/problems/path-sum/discuss/36378/AcceptedMy-recursive-solution-in-Java
+        static bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null) return false;
+            if (root.left == null && root.right == null) return sum == root.val;
+            return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+        }
+    }
+}
